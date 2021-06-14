7dc0fbd9f868717c9f01f2fca59c30b4216467e1
TurtleGITHUB <76065011+TurtleGITHUB@users.noreply.github.com>
2021-06-14 09:54:16 -0400

    Original upload

---
+++ PirateTalk.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>How to Talk Like a Pirate</title>
+</head>
+<body>
+	<h1>How to Talk Like a Pirate</h1>
+	<?php
+		$day = "";
+		echo "<p>Avast me hearties!<br>Return handsomely with some fine swag, ye scurvy dogs!<br>Else, we be keelhaulin' ye' next $day...</p>";
+	?>
+</body>
+</html>
